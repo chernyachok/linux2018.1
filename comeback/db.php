ecb2413527b7dd2a76f011d3d910de899b6cffc6
Yuriy <29680062+chernyachok@users.noreply.github.com>
2018-09-08 14:03:54 +0300

    Update db.php

--- comeback/db.php
+++ comeback/db.php
@@ -1,7 +1,7 @@
 <?php
  require "libs/rb.php";
   R::setup( 'mysql:host=localhost; dbname=id5519891_users',
-      'id5519891_yurochka', 'tyson2033' ); //for both mysql or mariaDB
+      'id5519891_yurochka', '' ); //for both mysql or mariaDB
    //R::setup( 'mysql:host=localhost; dbname=messenger',
    // 'root', '123' ); 
 	if(!R::testConnection()){
